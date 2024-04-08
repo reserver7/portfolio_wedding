f721b84270b9c223c113e0a5c718273cb2794e19
reserver7 <reserver7@gmail.com>
2024-04-08 09:35:12 +0900

    feat: kakao 피드 공유하기

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Invitation from './components/sections/Invitation'
 import Calendar from './components/sections/Calendar'
 import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
+import Share from './components/sections/Share'
 
 const cx = classNames.bind(styles)
 
@@ -82,6 +83,7 @@ function App() {
       <Calendar date={date} />
       <Map location={location} />
       <Contact groom={groom} bride={bride} />
+      <Share groomName={groom.name} brideName={bride.name} date={date} />
     </div>
   )
 }
